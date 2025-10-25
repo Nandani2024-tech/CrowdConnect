f42275947a9b7b760f63f44c41ff4773e24a4df3
Nandani2024-tech <nandani.24bcs10317@sst.scaler.com>
2025-10-25 21:21:47 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     'http://localhost:5173',  // Local development
-    'https://crowd-connect-h4ga.vercel.app'  // Production frontend
+    'https://crowd-connect-delta.vercel.app'  // Production frontend
   ],
   credentials: true
 }));
